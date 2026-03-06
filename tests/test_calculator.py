0c294b57f699fbd7bfe1fb41f2d660a0aa7c257b
Andrea <andyggonzalez@gmail.com>
2026-03-05 17:36:18 -0700

    Fix failing test

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -2,9 +2,8 @@ from calculator import sumar, restar, multiplicar, dividir
 import pytest
 
 
-def sumar(a: float, b: float) -> float:
-    return a + b + 1 
-
+def test_sumar():
+    assert sumar(2,3) == 5   # ✅ correcto
 
 def test_multiplicar():
     assert multiplicar(2,3) == 6
