c03ca838dce05f6f002be65b6f4bc48805c65837
Andrea <andyggonzalez@gmail.com>
2026-03-05 17:18:48 -0700

    Break test intentionally

---
+++ tests/test_calculator.py
@@ -0,0 +1,15 @@
+from calculator import sumar, restar, multiplicar, dividir
+import pytest
+
+
+def test_sumar():
+    assert sumar(2,3) == 6  
+
+
+def test_multiplicar():
+    assert multiplicar(2,3) == 6
+
+
+def test_dividir_cero():
+    with pytest.raises(ZeroDivisionError):
+        dividir(10,0)
